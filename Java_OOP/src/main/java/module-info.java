c0b74dbf459d05e92f45043420e498b3f804d512
Diogo <diogovsmartins>
2022-01-06 20:54:53 -0300

    Enviando os arquivos dos estudos. 06/01/2022

--- Java_OOP/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.java_oop {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bytebank_encapsulado to javafx.fxml;
-    exports Bytebank_encapsulado;
-    opens Bytebank_encapsulado to javafx.fxml;
-}
